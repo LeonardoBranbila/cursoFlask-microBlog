d02af13935ded932470ada32bbe32796fd13e858
Leonardo B <branbila.leo@gmail.com>
2024-01-02 21:25:59 -0300

    Incluindo imagem, css, rota para metodo post

--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,24 @@
 from app import app
 from flask import render_template
+from flask import request
 
 @app.route('/')
-@app.route('/index')
-def index():
-    nome = "Leonardo"
-    dados = {"cargo": "Vagabundo", "empresa":"Wipro"}
+@app.route('/index', defaults={"nome":"usuário"}) #Chumba valor default
+@app.route('/index/<nome>')
+def index(nome):
+    dados = {"cargo": "Vagabundo", "empresa":"casa"}
     return render_template('index.html', nome=nome, dados=dados)
 
 @app.route('/contato')
 def contato():
     return render_template('contato.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/autenticar', methods=['POST'])
+def autenticar():
+    usuario = request.form.get('usuario')
+    senha = request.form.get('senha')
+    return "Usuario logado"
